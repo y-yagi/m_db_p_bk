6f24e11adb41fc20f1a2a65fe3c280fa2c2e967d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2019-05-20 08:37:04 +0900

    add `database_selector` notification

--- config/initializers/notification.rb
+++ config/initializers/notification.rb
@@ -6,4 +6,9 @@ if Rails.env.development?
     config = event.payload[:connection].instance_variable_get(:@config)
     logger.info("[#{event.name}] time: #{event.duration.to_f} sql: #{sql} config: #{config} role: #{role}")
   end
+
+  # database_selector.active_record.read_from_primary, database_selector.active_record.read_from_replica, database_selector.active_record.wrote_to_primary
+  ActiveSupport::Notifications.subscribe(/database_selector/) do |event|
+    logger.info("[#{event.name}] time: #{event.duration.to_f}")
+  end
 end
