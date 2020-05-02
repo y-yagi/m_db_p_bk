cbe426b3b1a2347344003ba52bd7c77456f914c0
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2020-05-02 16:31:15 +0900

    Use PostgreSQL 12

--- containers/db_replica/docker-entrypoint.sh
+++ containers/db_replica/docker-entrypoint.sh
@@ -19,10 +19,9 @@ echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 
 set -e
 
-cat > ${PGDATA}/recovery.conf <<EOF
-standby_mode = on
+cat > ${PGDATA}/postgresql.conf <<EOF
 primary_conninfo = 'host=pg_master port=5432 user=$PG_REP_USER password=$PG_REP_PASSWORD'
-trigger_file = '/tmp/touch_me_to_promote_to_me_master'
+promote_trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
 chown postgres. ${PGDATA} -R
 chmod 700 ${PGDATA} -R
